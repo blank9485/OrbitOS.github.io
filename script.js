ac91dd5949d10c3a58beb03f08755f6d0c3581c6
Blank9485 <137871802+blank9485@users.noreply.github.com>
2024-08-06 14:22:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -32,19 +32,19 @@ function handleCommand(command) {
       outputElement.textContent += 'Kernel: GeminiKernel 5.0.0-mvm\n';
       outputElement.textContent += 'OS type: Linux\n';
       outputElement.textContent += 'Host: localhost\n';
-      outputElement.textContent += 'Distro: OrbitOS\n'; // Added Distro information
-      outputElement.textContent += 'CPU: (4) x Intel Core i7-8700K @ 3.70GHz\n'; // Added CPU information
-      outputElement.textContent += 'Memory: 16GB\n'; // Added Memory information
-      outputElement.textContent += 'GPU: NVIDIA GeForce GTX 1080 Ti\n'; // Added GPU information
+      outputElement.textContent += 'Distro: OrbitOS\n';
+      outputElement.textContent += 'CPU: (4) x Intel Core i7-8700K @ 3.70GHz\n';
+      outputElement.textContent += 'Memory: 16GB\n';
+      outputElement.textContent += 'GPU: NVIDIA GeForce GTX 1080 Ti\n';
       break;
     case 'software-update':
       outputElement.textContent += 'Searching for updates.....\n';
       setTimeout(() => {
-        outputElement.textContent += 'Last update: 06.08.2024\nVersion 1.3:\n-added echo command and made some changes\n\n';
+        outputElement.textContent += 'Last update: 06.08.2024\nVersion 1.3:\n-added echo and made some changes to neofetch command.\n\n';
       }, 3000);
       break;
     case 'help':
-      outputElement.textContent += 'Available commands:\n- neofetch\n- software-update\n- calc <expression>\n- help\n- date\n- time\n- clear\n- ls\n- echo <text>\n'; // Added echo command
+      outputElement.textContent += 'Available commands:\n- neofetch\n- software-update\n- calc <expression>\n- help\n- date\n- time\n- clear\n- ls\n- echo <text>\n';
       break;
     case 'date':
       const currentDate = new Date();
@@ -63,11 +63,7 @@ function handleCommand(command) {
       outputElement.textContent += 'documents downloads music pictures public videos\n';
       break;
     case 'echo':
-      if (command.length > 4) {
-        outputElement.textContent += command.substring(5) + '\n';
-      } else {
-        outputElement.textContent += 'Usage: echo <text>\n';
-      }
+      outputElement.textContent += command.substring(5) + '\n';
       break;
     default:
       if (command.startsWith('calc ')) {
